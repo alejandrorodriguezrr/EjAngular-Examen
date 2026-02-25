5f293514071f72bde4aea22800ae2c4a2d3166d0
alejandrorodriguezrr <alexrodrod2003@gmail.com>
2026-02-25 10:49:05 +0100

    hecho

--- frontend/src/app/components/latder/latder.ts
+++ frontend/src/app/components/latder/latder.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Carrito } from "../carrito/carrito";
+import { Hijo } from '../hijo/hijo';
+import { Padre } from '../padre/padre';
 
 @Component({
   selector: 'app-latder',
-  imports: [Carrito],
+  imports: [Carrito, Padre, Hijo],
   templateUrl: './latder.html',
   styleUrl: './latder.css',
 })
