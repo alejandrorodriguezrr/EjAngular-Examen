25ba1f80a181524456e0b105d5c79c22adc9966b
alejandrorodriguezrr <alexrodrod2003@gmail.com>
2026-03-03 00:10:32 +0100

    hecho

--- frontend/src/app/components/categorias/categorias.ts
+++ frontend/src/app/components/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { ComprasServices } from '../../services/compras-services';
+import { ClientesServices } from '../../services/clientes-services';
 
 @Component({
   selector: 'app-categorias',
@@ -17,7 +19,7 @@ export class Categorias implements OnInit {
 
   private urlCategorias = 'http://localhost:5050/api/libros/generos';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private comprasService:ComprasServices, private clientesServices: ClientesServices) {}
 
   ngOnInit(): void {
     this.cargarCategorias();
@@ -31,6 +33,38 @@ export class Categorias implements OnInit {
       },
       error: (e) => console.log('Error cargando categorias', e)
     });
+  }
+
+  clientesPrimeraCategoria: any[] = []
+
+  mostrarNombres(){
+
+    this.http.get<string[]>(this.urlCategorias).subscribe({
+      next: (datos: any) => {
+        const primeraCategoria=this.categorias[0]
+
+        this.comprasService.mostrarCompras().subscribe({
+          next: (compras:any) => {
+            const idsClientes: string[] = []
+            compras.forEach((compra:any) => {
+              compra.libros.forEach((libro: any) => {
+                if(libro.genero==primeraCategoria){
+                  idsClientes.push(compra.clienteId)
+                }
+              });
+            });
+
+            idsClientes.forEach((id:string) => {
+              this.clientesServices.mostrarCliente(id).subscribe({
+                next: (cliente:any) => {
+                  this.clientesPrimeraCategoria.push(cliente.nombre)
+                }
+              })
+            })
+          }
+        })
+      }
+    })
   }
 
   seleccionarCategoria(categoria: string): void {
