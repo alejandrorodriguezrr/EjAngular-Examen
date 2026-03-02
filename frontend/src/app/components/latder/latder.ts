63f6f62175e91b3469cc338ad2b89fd1eef0bed0
alejandrorodriguezrr <alexrodrod2003@gmail.com>
2026-03-02 12:56:12 +0100

    hecho

--- frontend/src/app/components/latder/latder.ts
+++ frontend/src/app/components/latder/latder.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { Carrito } from "../carrito/carrito";
+import { FormsModule } from '@angular/forms';
+import { ComprasServices } from '../../services/compras-services';
 
 @Component({
   selector: 'app-latder',
-  imports: [Carrito],
+  imports: [Carrito,FormsModule],
   templateUrl: './latder.html',
   styleUrl: './latder.css',
 })
 export class Latder {
+
+  productoabuscar:string=""
+  contador:number=0
+
+  constructor(private comprasService: ComprasServices){}
+
+  buscador(){
+    this.comprasService.mostrarCompras().subscribe({
+      next: (compras:any)=>{
+        let total=0
+
+        compras.forEach((compra:any) => {
+            compra.libros.forEach((libro:any) => {
+              if(this.productoabuscar===libro.titulo){
+                total++
+              }
+            });
+        this.contador=total
+        });
+      }
+    })
+  }
 
 }
