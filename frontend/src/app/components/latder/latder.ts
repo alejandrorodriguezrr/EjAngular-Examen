4e806e64e88289095af1935074a762dae8bb7796
alejandrorodriguezrr <alexrodrod2003@gmail.com>
2026-03-03 00:48:59 +0100

    hecho

--- frontend/src/app/components/latder/latder.ts
+++ frontend/src/app/components/latder/latder.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Carrito } from "../carrito/carrito";
+import { ComprasServices } from '../../services/compras-services';
 
 @Component({
   selector: 'app-latder',
@@ -8,5 +9,45 @@ import { Carrito } from "../carrito/carrito";
   styleUrl: './latder.css',
 })
 export class Latder {
+
+  constructor(private comprasServices: ComprasServices){}
+
+  nombreProducto: string=""
+  precio: number=0
+
+  ngOnInit(){
+
+    const userRaw = localStorage.getItem('user');
+    if (!userRaw) {
+      alert('Debes iniciar sesión para realizar una compra');
+      return;
+    }
+
+    const cliente = JSON.parse(userRaw);
+    const clienteId = cliente._id ?? cliente.id;
+
+    if (!clienteId) {
+      console.error('Usuario sin ID:', cliente);
+      alert('Error: el usuario logueado no tiene ID. Revisa el login.');
+      return;
+    }
+
+    this.comprasServices.mostrarComprasCliente(clienteId).subscribe({
+      next: (compras:any) => {
+        let MaxPrecio=0
+
+        compras.forEach((compra:any) => {
+          compra.libros.forEach((libro:any) => {
+            if(libro.precio>MaxPrecio){
+              MaxPrecio=libro.precio
+              this.nombreProducto=libro.titulo
+            }
+          });
+        });
+        this.precio=MaxPrecio
+      }
+    })
+
+  }
 
 }
