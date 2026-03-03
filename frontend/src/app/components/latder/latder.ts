f8481fdd0f8f8489138ab3cf25670596cc777217
alejandrorodriguezrr <alexrodrod2003@gmail.com>
2026-03-03 10:56:56 +0100

    ni idea

--- frontend/src/app/components/latder/latder.ts
+++ frontend/src/app/components/latder/latder.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Carrito } from "../carrito/carrito";
+import { ComprasServices } from '../../services/compras-services';
+import { ClientesServices } from '../../services/clientes-services';
 
 @Component({
   selector: 'app-latder',
@@ -9,4 +11,24 @@ import { Carrito } from "../carrito/carrito";
 })
 export class Latder {
 
+  constructor(private comprasServices: ComprasServices, private clientesServices: ClientesServices){}
+
+  ngOnInit(){
+
+    this.comprasServices.mostrarCompras().subscribe({
+      next: (compras:any) => {
+        let MaxPedidos=0
+        let topId=""
+
+        compras.forEach((compra:any) => {
+          let pedidosCliente=0
+          compras.forEach((c:any) => {
+            if(c.clienteId === compra.clienteId){
+              pedidosCliente+++
+            }
+          });
+        });
+      }
+    })
+  }
 }
