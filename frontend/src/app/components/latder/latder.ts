fff01edca4af85eb3be150e02c45a492d23c720f
alejandrorodriguezrr <alexrodrod2003@gmail.com>
2026-03-03 10:12:22 +0100

    hecho

--- frontend/src/app/components/latder/latder.ts
+++ frontend/src/app/components/latder/latder.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { Carrito } from "../carrito/carrito";
+import { ComprasServices } from '../../services/compras-services';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-latder',
-  imports: [Carrito],
+  imports: [Carrito, NgFor],
   templateUrl: './latder.html',
   styleUrl: './latder.css',
 })
 export class Latder {
+  
+  constructor(private comprasServices: ComprasServices){}
 
+  ultimaCompra:string=""
+  ultimoLibro:any[]=[]
+
+  ngOnInit(){
+    this.comprasServices.mostrarCompras().subscribe({
+      next: (compras:any) => {
+        const ultimaCompra = compras[compras.length-1]
+        const ultimolibro = ultimaCompra.libros[ultimaCompra.libros.length-1]
+
+        for(let i=0;i<ultimolibro.cantidad;i++){
+          this.ultimoLibro.push(ultimolibro.titulo)
+        }
+        
+      }
+    })
+  }
 }
