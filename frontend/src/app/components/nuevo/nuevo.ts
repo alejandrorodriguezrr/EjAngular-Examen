f5792e7ec29cc8310caf83c3a536bc972fa9d8dc
alejandrorodriguezrr <alexrodrod2003@gmail.com>
2026-03-03 13:18:13 +0100

    hecho

---
+++ frontend/src/app/components/nuevo/nuevo.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { ComprasServices } from '../../services/compras-services';
+
+@Component({
+  selector: 'app-nuevo',
+  imports: [],
+  templateUrl: './nuevo.html',
+  styleUrl: './nuevo.css',
+})
+export class Nuevo {
+
+  constructor(private comprasServices: ComprasServices){}
+
+  nombreProducto: string=""
+
+  mostrar(){
+
+    this.comprasServices.mostrarCompras().subscribe({
+      next: (compras:any) => {
+        let MaxCaro=0
+        compras.forEach((compra:any) => {
+          compra.libros.forEach((libro:any) => {
+            if(libro.precio>MaxCaro){
+              MaxCaro=libro.precio
+              this.nombreProducto=libro.titulo
+            }
+          });
+        });
+      }
+    })
+
+  }
+
+}
