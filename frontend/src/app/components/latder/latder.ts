4145fd883a6a24a26bae05a87c98799be5cb570d
alejandrorodriguezrr <alexrodrod2003@gmail.com>
2026-03-03 16:36:54 +0100

    hecho

--- frontend/src/app/components/latder/latder.ts
+++ frontend/src/app/components/latder/latder.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Carrito } from "../carrito/carrito";
+import { ComprasServices } from '../../services/compras-services';
 
 @Component({
   selector: 'app-latder',
@@ -8,4 +9,34 @@ import { Carrito } from "../carrito/carrito";
   styleUrl: './latder.css',
 })
 export class Latder {
+
+  constructor(private comprasServices: ComprasServices ){}
+
+  primerProducto:any=null
+  ultimoProducto:any=null
+
+
+  ngOnInit(){
+    const userRaw = localStorage.getItem('user');
+    if (!userRaw) {
+      alert('Debes iniciar sesión para realizar una compra');
+      return;
+    }
+
+    const cliente = JSON.parse(userRaw);
+    const clienteId = cliente._id ?? cliente.id;
+
+    if (!clienteId) {
+      console.error('Usuario sin ID:', cliente);
+      alert('Error: el usuario logueado no tiene ID. Revisa el login.');
+      return;
+    }
+
+    this.comprasServices.mostrarComprasCliente(clienteId).subscribe({
+      next: (compras:any) => {
+        this.primerProducto=compras[0].libros[0]
+        this.ultimoProducto=compras[compras.length-1].libros[compras[compras.length-1].libros.length-1]
+      }
+    })
+  }
 }
