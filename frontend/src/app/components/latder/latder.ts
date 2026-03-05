61db7eec62d15bdd49f0c2e8f675619a83cd185d
alejandrorodriguezrr <alexrodrod2003@gmail.com>
2026-03-05 12:38:39 +0100

    hecho

--- frontend/src/app/components/latder/latder.ts
+++ frontend/src/app/components/latder/latder.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
 import { Carrito } from "../carrito/carrito";
+import { ClientesServices } from '../../services/clientes-services';
+import { ComprasServices } from '../../services/compras-services';
+import { NgForOf } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-latder',
-  imports: [Carrito],
+  imports: [Carrito, NgFor],
   templateUrl: './latder.html',
   styleUrl: './latder.css',
 })
 export class Latder {
+
+  constructor(private clientesServices: ClientesServices, private comprasServices:ComprasServices){}
+
+  clientes: any[]=[]
+
+  ngOnInit(){
+
+    this.clientesServices.mostrarClientes().subscribe({
+      next:(clientes:any) => {
+        clientes.forEach((cliente:any) => {
+          this.comprasServices.mostrarComprasCliente(cliente._id).subscribe({
+            next:(compras:any) => {
+              let categorias:string[]=[]
+              compras.forEach((compra:any) => {
+                compra.libros.forEach((libro:any) => {
+                  console.log('generos', libro.genero)
+                  if(!categorias.includes(libro.genero)){
+                    categorias.push(libro.genero)
+                  }
+                });
+              });
+              this.clientes.push({
+                nombre: cliente.nombre,
+                numero: categorias.length
+              })
+            }
+          })
+        });
+      }
+    })
+
+  }
+
 }
