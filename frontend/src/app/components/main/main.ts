6fac1d950bd55b87319b069ff0927091d1664ca8
alejandrorodriguezrr <alexrodrod2003@gmail.com>
2026-03-12 12:43:42 +0100

    mal

--- frontend/src/app/components/main/main.ts
+++ frontend/src/app/components/main/main.ts
@@ -4,13 +4,35 @@ import { Latizq } from "../latizq/latizq";
 import { Medio } from "../medio/medio";
 import { Latder } from "../latder/latder";
 import { Footer } from "../footer/footer";
+import { LibrosServices } from '../../services/libros-services';
+import { ComprasServices } from '../../services/compras-services';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-main',
-  imports: [Head, Latizq, Medio, Latder, Footer],
+  imports: [Head, Latizq, Medio, Latder, Footer, NgFor],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
 export class Main {
+
+  nombreProductos:string[]=[]
+  repetidos:string[]=[]
+
+  constructor(private comprasServices:ComprasServices){}
+
+  ngOnInit(){
+    this.comprasServices.mostrarCompras().subscribe({
+      next: (compras:any) => {
+        compras.forEach((compra:any) => {
+          compra.libros.forEach((libro:any) => {
+            if(this.nombreProductos.includes(libro.titulo)){
+              
+            }
+          });
+        });
+      }
+    })
+  }
 
 }
