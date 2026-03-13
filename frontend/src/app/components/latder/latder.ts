81393fbadd849897e0a13b196690234a676f90d2
alejandrorodriguezrr <alexrodrod2003@gmail.com>
2026-03-13 10:38:50 +0100

    bien

--- frontend/src/app/components/latder/latder.ts
+++ frontend/src/app/components/latder/latder.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Carrito } from "../carrito/carrito";
+import { ClientesServices } from '../../services/clientes-services';
+import { ComprasServices } from '../../services/compras-services';
 
 @Component({
   selector: 'app-latder',
@@ -8,4 +10,37 @@ import { Carrito } from "../carrito/carrito";
   styleUrl: './latder.css',
 })
 export class Latder {
+
+  constructor(private clienteServices:ClientesServices, private comprasServices:ComprasServices){}
+
+  nombreCliente=""
+  totalProductos=0
+
+  ngOnInit(){
+    this.clienteServices.mostrarClientes().subscribe({
+      next: (clientes:any) => {
+        let MaxCompras=0
+        clientes.forEach((cliente:any) => {
+          this.comprasServices.mostrarComprasCliente(cliente._id).subscribe({
+            next: (compras:any) => {
+              let totalP=0
+
+              if(compras.length>MaxCompras){
+                MaxCompras=compras.length
+                this.nombreCliente=cliente.nombre
+
+                compras.forEach((compra:any) => {
+                compra.libros.forEach((libro:any) => {
+                  this.totalProductos+=libro.cantidad
+                });
+              });
+              }      
+            }
+          })
+        });
+      }
+    })
+  }
+
+
 }
